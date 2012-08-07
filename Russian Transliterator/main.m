add8bbd8610029f622cdd8ff4495d64e07307413
Oleg Utkin <utkin.oleg@me.com>
2012-08-07 05:22:38 -0700

    Initial Commit

---
+++ Russian Transliterator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Russian Transliterator
+//
+//  Created by Oleg Utkin on 8/7/12.
+//  Copyright (c) 2012 akaiBureido. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
